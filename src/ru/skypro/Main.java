91d6d9a1b1c6438bbc5a0ecd5e8df0e1b0cb8bd7
Vita <Vitayliyost@mail.ru>
2022-02-02 02:41:13 +0300

    выполнила дз номер 9

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,14 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Author tolstoy = new Author("Lev", "Tolstoy");
+        Author rowling = new Author("Joanne", "Rowling");
+
+	Book annaKarenina = new Book("Anna Karenina", "1873", tolstoy);
+    Book garryPotter = new Book("Garry Potter", "2000", rowling);
+
+    garryPotter.setPublishingYear(1998);
+
     }
 }
